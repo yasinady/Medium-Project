b2981c73274b46a26c85704782326da21ae2cc2d
Yasin <kurucuu@hotmail.com>
2022-12-31 00:04:28 +0300

    html file changed

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('.header')
+const button = document.getElementById('btnHeader')
+window.onscroll = function(){
+    var top = window.scrollY
+    console.log(top.toFixed());
+    if(top >= 340){
+        header.classList.add('changeMenu')
+        button.classList.add('btnColor')
+    }else{
+        header.classList.remove('changeMenu')
+        button.classList.remove('btnColor')
+    }
+} 
